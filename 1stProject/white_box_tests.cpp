e6de1cc77914a0319c7bf97f22625b9d6e175993
Ja <lukashais@disroot.org>
2021-02-18 20:05:57 +0100

    more coverage

--- 1stProject/white_box_tests.cpp
+++ 1stProject/white_box_tests.cpp
@@ -28,23 +28,26 @@
 TEST(Matrix, Initialization)
 {
   Matrix m1;
-  ASSERT_TRUE(m1.get(0, 0) == 0);
-  ASSERT_FALSE(m1.get(0, 0) == 1);
-
   Matrix m2(3, 3);
-  ASSERT_TRUE(m2.get(0, 0) == 0);
-  ASSERT_TRUE(m2.get(2, 2) == 0);
-  ASSERT_FALSE(m2.get(1, 1) == 3);
+
+  try
+  {
+    Matrix m3(0, 0);
+  }
+  catch(const std::exception& e)
+  {
+    ASSERT_TRUE(true);
+  }
 }
 
 TEST(Matrix, SetOneValue)
 {
   Matrix m1;
-  ASSERT_TRUE(m1.get(0, 0) == 0);
   ASSERT_TRUE(m1.set(0, 0, 5));
 
   Matrix m2(3, 3);
   ASSERT_TRUE(m2.set(2, 2, 10));
+  ASSERT_FALSE(m2.set(4, 4, 5));
 }
 
 TEST(Matrix, SetMultipleValues)
@@ -52,7 +55,12 @@ TEST(Matrix, SetMultipleValues)
   Matrix m1(3,3);
 
   std::vector<std::vector<double> > val1{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+  std::vector<std::vector<double> > val2{{0, 1, 2}, {3, 4, 5}};
+  std::vector<std::vector<double> > val3{{0, 1}, {4, 5}, {8}};
+
   ASSERT_TRUE(m1.set(val1));
+  ASSERT_FALSE(m1.set(val2));
+  ASSERT_FALSE(m1.set(val3));
 }
 
 TEST(Matrix, GetValues)
@@ -63,6 +71,15 @@ TEST(Matrix, GetValues)
   ASSERT_TRUE(m1.get(0, 1) == 1);
   ASSERT_TRUE(m1.get(1, 1) == 4);
   ASSERT_TRUE(m1.get(2, 1) == 7);
+
+  try
+  {
+    m1.get(4, 4);
+  }
+  catch(const std::exception& e)
+  {
+    ASSERT_TRUE(true);
+  }
 }
 
 TEST(Matrix, EqualMatrices)
@@ -70,16 +87,27 @@ TEST(Matrix, EqualMatrices)
   Matrix m1(3, 4);
   Matrix m2(3, 4);
   Matrix m3(3, 4);
+  Matrix m4(4, 4);
   m3.set(0, 0, 35);
 
   ASSERT_TRUE(m1.operator==(m2));
   ASSERT_FALSE(m1.operator==(m3));
+
+  try
+  {
+    m1.operator==(m4);
+  }
+  catch(const std::exception& e)
+  {
+    ASSERT_TRUE(true);
+  }
 }
 
 TEST(Matrix, AddingMatrices)
 {
   Matrix m1(2, 2);
   Matrix m2(2, 2);
+  Matrix m4(3, 3);
 
   std::vector<std::vector<double> > val1{{2, 4}, {4, 2}};
   std::vector<std::vector<double> > val2{{1, 1}, {1, 1}};
@@ -90,6 +118,15 @@ TEST(Matrix, AddingMatrices)
 
   ASSERT_TRUE(m3.get(0, 0) == 3);
   ASSERT_TRUE(m3.get(1, 0) == 5);
+
+  try
+  {
+    m1.operator*(m4);
+  }
+  catch(const std::exception& e)
+  {
+    ASSERT_TRUE(true);
+  }
 }
 
 TEST(Matrix, MultiplyMatrices)
@@ -97,6 +134,7 @@ TEST(Matrix, MultiplyMatrices)
 
   Matrix m1(2, 2);
   Matrix m2(2, 2);
+  Matrix m5(4, 4);
 
   std::vector<std::vector<double> > val1{{2, 4}, {4, 2}};
   std::vector<std::vector<double> > val2{{2, 3}, {4, 5}};
@@ -112,6 +150,15 @@ TEST(Matrix, MultiplyMatrices)
   ASSERT_TRUE(m3.get(1, 1) == 22);
 
   ASSERT_TRUE(m3.get(1, 1) != m4.get(1, 1));
+
+  try
+  {
+    m1.operator*(m4);
+  }
+  catch(const std::exception& e)
+  {
+    ASSERT_TRUE(true);
+  }
 }
 
 TEST(Matrix, MultiplyMatrixByConstant)
@@ -140,13 +187,18 @@ TEST(Matrix, EquationSolving)
 {
   Matrix m1(3, 3);
   Matrix m2(2, 2);
+  Matrix m3(2, 3);
+  Matrix m4(2, 2);
   std::vector<std::vector<double> > val1{{2, 3, 7}, {3, 8, 1}, {0, 3, 3}};
   std::vector<double> right1{47, 50, 27};
   std::vector<std::vector<double> > val2{{4, 3}, {2, 1}};
   std::vector<double> right2{6, 4};
+  std::vector<double> wrongRight{1};
+  std::vector<std::vector<double> > singVal{{1, 1}, {1, 1}};
 
   ASSERT_TRUE(m1.set(val1));
   ASSERT_TRUE(m2.set(val2));
+  ASSERT_TRUE(m4.set(singVal));
 
   std::vector<double> result1 = m1.solveEquation(right1);
   std::vector<double> result2 = m2.solveEquation(right2);
@@ -157,6 +209,34 @@ TEST(Matrix, EquationSolving)
 
   ASSERT_TRUE(result2[0] == 3);
   ASSERT_TRUE(result2[1] == -2);
+
+  // right num of elements != left rows
+  try
+  {
+    m1.solveEquation(wrongRight);
+  }
+  catch (const std::runtime_error& e)
+  {
+    ASSERT_TRUE(true);
+  }
+  // matrix not NxN
+  try
+  {
+    m3.solveEquation(right2);
+  }
+  catch (const std::runtime_error& e)
+  {
+    ASSERT_TRUE(true);
+  }
+  // singular matrix
+  try
+  {
+    m4.solveEquation(right2);
+  }
+  catch (const std::runtime_error& e)
+  {
+    ASSERT_TRUE(true);
+  }
 }
 
 TEST(Matrix, Transpose)
@@ -185,11 +265,15 @@ TEST(Matrix, Inverse)
 {
   Matrix m1(2, 2);
   Matrix m2(3, 3);
+  Matrix m5(4, 4);
+  Matrix m6(2, 2);
   std::vector<std::vector<double> > val1{{1, 2}, {3, 4}};
   std::vector<std::vector<double> > val2{{9, 9, 9}, {4, 5, 6}, {3, 3, 9}};
+  std::vector<std::vector<double> > wrongVal{{1, 2}, {2, 4}};
   
   ASSERT_TRUE(m1.set(val1));
   ASSERT_TRUE(m2.set(val2));
+  ASSERT_TRUE(m6.set(wrongVal));
 
   Matrix m3 = m1.inverse();
   Matrix m4 = m2.inverse();
@@ -202,5 +286,22 @@ TEST(Matrix, Inverse)
   ASSERT_TRUE(m4.get(0, 1) == -1);
   ASSERT_TRUE(m4.get(1, 1) == 1);
   ASSERT_TRUE(m4.get(2, 1) == 0);
+
+  try
+  {
+    m5.inverse();
+  }
+  catch (const std::exception& e)
+  {
+    ASSERT_TRUE(true);
+  }
+  try
+  {
+    m6.inverse();
+  }
+  catch (const std::exception&)
+  {
+    ASSERT_TRUE(true);
+  }
 }
 /*** Konec souboru white_box_tests.cpp ***/
